0d8e59899b542deca4448a8d312f124c4a34f8a2
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2022-04-21 09:05:28 +0300

    added navbar

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TopNav from './TopNav';
+
+const Layout = (): any => {
+  return (
+    <>
+      <TopNav />
+      <div>Layout</div>
+    </>
+  );
+};
+
+export default Layout;
---
+++ components/TopNav.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const TopNav = () => {
+  return (
+    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+      HAVBAR
+    </nav>
+  );
+};
+
+export default TopNav;
