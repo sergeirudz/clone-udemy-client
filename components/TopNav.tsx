e42f2c0c272ef9d1d5680e9b404f49602d334f1a
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2022-04-22 08:11:35 +0300

    added Sass

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './TopNav.module.css';
+import styles from './TopNav.module.scss';
 
 const TopNav = () => {
-  return <nav className={styles.container}>HAVBAR</nav>;
+  return (
+    <nav className={styles.container}>
+      <h1 className={styles.title}>NAV BAR</h1>
+    </nav>
+  );
 };
 
 export default TopNav;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
-import Layout from '../components/Layout';
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
