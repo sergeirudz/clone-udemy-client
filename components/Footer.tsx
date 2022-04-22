1866cf7fa297e5678357f90b31cbf4791e45829c
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2022-04-22 08:39:09 +0300

    added Layout and Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>FOOTER</h1>
+    </div>
+  );
+};
+
+export default Footer;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Footer from './Footer';
 import TopNav from './TopNav';
 
-const Layout = (): any => {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+/* 
+TS doc. https://dev.to/fromaline/jsxelement-vs-reactelement-vs-reactnode-2mh2
+*/
+
+const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
   return (
     <>
       <TopNav />
-      <div>Layout</div>
+      {children}
+      <Footer />
     </>
   );
 };
